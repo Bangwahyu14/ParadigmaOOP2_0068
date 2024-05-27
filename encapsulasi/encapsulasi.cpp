f4230bf4cc497c9ba066e1d4d3edd7ede80dac42
Bangwahyu14 <whagung553@gmail.com>
2024-05-27 15:37:19 +0700

    menambahkan lampu rumah dan cout lampu rumah

--- encapsulasi/encapsulasi.cpp
+++ encapsulasi/encapsulasi.cpp
@@ -14,5 +14,16 @@ public:
 };
 
 int main() {
-	remoteLampu
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah, setSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah, setSaklarNo(2, "Lampu Kamar Tidur");
+	lampuRumah, setSaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getsaklarNo(0, "Lampu Teras Rumah");
+	cout << lampuRumah.getsaklarNo(1, "Lampu Ruang Tamu");
+	cout << lampuRumah.getsaklarNo(2, "Lampu Kamar Tidur");
+	cout << lampuRumah.getsaklarNo(3, "Lampu Dapur");
+
 }
